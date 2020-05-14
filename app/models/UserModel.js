830c6dce326a14c1c5e28ba1b731301603022a2f
shindepriti <pritishinde34@gmail.com>
2020-05-14 21:45:31 +0530

    ADDED:Check Email validate Using Regx

--- app/models/UserModel.js
+++ app/models/UserModel.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     firstName:{type : String,required : true,minlength:3},
     lastName:{type : String,required : true,minlength:3},
-    emailId:{ type : String ,required : true,unique:true},
-    password:{ type : String,required : true,minlength:8},
+    emailId:{type : String ,required : true,unique:true,match:[/^([a-zA-Z]{3,}([._+-]?[a-zA-Z0-9])*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}[.]?[a-zA-Z]*)$/,'is invalid']},
+    password:{type : String,required : true,minlength:8},
 },{
         timestamps:true
 
